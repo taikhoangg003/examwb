7c39f177073ddafd8bd1843e89a28535b7f76dcd
nvngoctrai <taikhoangg002@gmail.com>
2019-09-29 09:45:07 +0000

    fix oauth heroku

--- database/migrations/2019_09_27_151148_create_role_users_table.php
+++ database/migrations/2019_09_27_151148_create_role_users_table.php
@@ -13,11 +13,11 @@ class CreateRoleUsersTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('role_users');
         Schema::create('role_users', function (Blueprint $table) {
             $table->unsignedInteger('user_id');
             $table->unsignedInteger('role_id');
             $table->timestamps();
-
             $table->unique(['user_id','role_id']);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
